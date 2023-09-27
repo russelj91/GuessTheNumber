7b4ff0744748e6a833751bfad100a818c7d5235d
john Russel <russelj91@gmail.com>
2023-09-27 22:00:50 +0800

    1

--- script.js
+++ script.js
@@ -24,8 +24,9 @@ document.querySelector(".check").addEventListener("click", function () {
       highscore = score;
       document.querySelector(".topscore").textContent = highscore;
     }
-  } else if (guess > secretNumber) {
-    document.querySelector(".message").textContent = "Number Too High!";
+  } else if (guess !== secretNumber) {
+    document.querySelector(".message").textContent =
+      guess > secretNumber ? "Number Too High!" : "Number Too Low!";
     if (score > 1) {
       score--;
       document.querySelector(".score").textContent = score;
@@ -47,8 +48,31 @@ document.querySelector(".check").addEventListener("click", function () {
   }
 });
 
+//   else if (guess > secretNumber || guess < secretNumber) {
+//     document.querySelector(".message").textContent = "Number Too High!";
+//     if (score > 1) {
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//     } else {
+//       document.querySelector(".number").textContent = secretNumber;
+//       document.querySelector(".message").textContent = "You Lose The Game!";
+//       document.querySelector("body").style.backgroundColor = "red";
+//     }
+//   } else if (guess < secretNumber) {
+//     if (score > 1) {
+//       document.querySelector(".message").textContent = "Number Too Low!";
+//       score--;
+//       document.querySelector(".score").textContent = score;
+//     } else {
+//       document.querySelector(".number").textContent = secretNumber;
+//       document.querySelector(".message").textContent = "You Lose The Game!";
+//       document.querySelector("body").style.backgroundColor = "red";
+//     }
+//   }
+// });
+
 document.querySelector(".reset").addEventListener("click", function () {
-  score = 20;
+  score = 10;
 
   document.querySelector(".number").textContent = "?";
   document.querySelector(".message").textContent = "Guess The Number!";
