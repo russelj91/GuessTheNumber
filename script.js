50d60708e265d8d141b82482bc2044dbbb432e03
john Russel <russelj91@gmail.com>
2023-09-27 22:24:41 +0800

    1

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 "use strict";
 
-// document.querySelector(".number").textContent = 5;
-// document.querySelector(".message").textContent = "You are Correct!";
-// document.querySelector(".score").textContent = 5;
-// document.querySelector(".guess").value = 10;
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 10;
-let highscore = 0;
 const displayMessage = function (message) {
   document.querySelector(".message").textContent = message;
 };
+
+let score = 10;
+let highscore = 0;
+let secretNumber;
+
+document.querySelector(".reset").addEventListener("click", function () {
+  score = 10;
+
+  displayMessage("Guess The Number!");
+  document.querySelector(".number").textContent = "?";
+
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+
+  // Generate a new secretNumber when resetting the game
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+});
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   if (!guess) {
     displayMessage("No Number!");
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent =
-      " You Got The Right Number";
+    displayMessage("You Got The Right Number");
     document.querySelector(".number").textContent = secretNumber;
     document.querySelector(".topscore").textContent = score;
     document.querySelector("body").style.backgroundColor = "green";
@@ -35,52 +45,11 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".number").textContent = secretNumber;
-      document.querySelector(".message").textContent = "You Lose The Game!";
-      document.querySelector("body").style.backgroundColor = "red";
-    }
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Number Too Low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".number").textContent = secretNumber;
-      document.querySelector(".message").textContent = "You Lose The Game!";
+      displayMessage("You Lose The Game!");
       document.querySelector("body").style.backgroundColor = "red";
     }
   }
 });
 
-//   else if (guess > secretNumber || guess < secretNumber) {
-//     document.querySelector(".message").textContent = "Number Too High!";
-//     if (score > 1) {
-//       score--;
-//       document.querySelector(".score").textContent = score;
-//     } else {
-//       document.querySelector(".number").textContent = secretNumber;
-//       document.querySelector(".message").textContent = "You Lose The Game!";
-//       document.querySelector("body").style.backgroundColor = "red";
-//     }
-//   } else if (guess < secretNumber) {
-//     if (score > 1) {
-//       document.querySelector(".message").textContent = "Number Too Low!";
-//       score--;
-//       document.querySelector(".score").textContent = score;
-//     } else {
-//       document.querySelector(".number").textContent = secretNumber;
-//       document.querySelector(".message").textContent = "You Lose The Game!";
-//       document.querySelector("body").style.backgroundColor = "red";
-//     }
-//   }
-// });
-
-document.querySelector(".reset").addEventListener("click", function () {
-  score = 10;
-
-  document.querySelector(".number").textContent = "?";
-  document.querySelector(".message").textContent = "Guess The Number!";
-  document.querySelector(".score").textContent = score;
-  document.querySelector(".guess").value = "";
-
-  document.querySelector("body").style.backgroundColor = "#222";
-});
+// Generate the initial secretNumber when the page loads
+secretNumber = Math.trunc(Math.random() * 20) + 1;
